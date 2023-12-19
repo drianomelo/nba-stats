6462474d1ef7f2eaadb670895055fe3ec9cf5d4d
Adriano <adrianosenny@gmail.com>
2023-12-19 11:31:40 -0300

    Feature: Create relationships between player->team and court->team

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,21 @@ class Team extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'city'];
+
+    public function players()
+    {
+        return $this->hasMany(Player::class, 'teams_id');
+    }
+
+    public function court()
+    {
+        return $this->hasOne(Court::class, 'teams_id');
+    }
+
+    protected static function booted()
+    {
+        self::addGlobalScope('order', function(Builder $queyBuilder) {
+            $queyBuilder->orderBy('city');
+        });
+    }
 }
